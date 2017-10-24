f0b48d8e03ffa429deb0a503114e1b8653dab6f6
Matthew McDonald <matthew.mcd1031@gmail.com>
2017-10-24 02:19:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function happyHolidaysTo(name){
   return "Happy holidays, you!"
 }
 function happyHolidayTo(holiday, name){
-  return "Happy Independence day, you!"
+  return "Happy Independence Day, you!"
 }
